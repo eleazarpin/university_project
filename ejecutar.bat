2c8e5555ab60b08a8cdbe378212a7d87d6a32418
Eleazar <eleazarpinetchave@gmail.com>
2018-11-02 20:49:07 -0300

    para que funcione en windows

--- ejecutar.bat
+++ ejecutar.bat
@@ -2,7 +2,7 @@ c:\GnuWin32\bin\bison -dyv sintactico.y
 c:\GnuWin32\bin\flex lexico.l
 
 c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o segunda.exe
-segunda.exe < prueba.txt
+segunda.exe prueba.txt
 @echo off
 del segunda.exe
 del lex.yy.c
